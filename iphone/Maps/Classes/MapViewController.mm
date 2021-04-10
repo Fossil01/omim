b53c773a06d285929922561393c1c4e8d4e055ad
Bento <bento@de-gier.eu>
2021-04-10 19:09:25 +0200

    Forgot to change the function called to its new name

--- iphone/Maps/Classes/MapViewController.mm
+++ iphone/Maps/Classes/MapViewController.mm
@@ -927,7 +927,7 @@ NSString *const kPP2BookmarkEditingSegue = @"PP2BookmarkEditing";
     [UIKeyCommand keyCommandWithInput:@"=" modifierFlags:UIKeyModifierCommand action:@selector(zoomIn)], // Alternative, not shown when holding CMD
     [UIKeyCommand keyCommandWithInput:@"+" modifierFlags:UIKeyModifierCommand action:@selector(zoomIn) discoverabilityTitle:@"Zoom In"],
     [UIKeyCommand keyCommandWithInput:UIKeyInputEscape modifierFlags:0 action:@selector(goBack) discoverabilityTitle:@"Go Back"],
-    [UIKeyCommand keyCommandWithInput:@"0" modifierFlags:UIKeyModifierCommand action:@selector(reCenterMap) discoverabilityTitle:@"Switch position mode"]
+    [UIKeyCommand keyCommandWithInput:@"0" modifierFlags:UIKeyModifierCommand action:@selector(switchPositionMode) discoverabilityTitle:@"Switch position mode"]
    ];
 }
 
